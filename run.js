2e486f15efd7e4a0719bf23907c336c0f87acec5
Yotam Lichter <yotmail@gmail.com>
2022-07-07 21:03:08 +0000

    p2

--- run.js
+++ run.js
@@ -2,21 +2,16 @@ const fs = require('fs');
 const lines = fs.readFileSync('list').toString().split('\n');
 let HK = []
 for (const line of lines) {
-    // HK = HK.concat(line.trim().split(','))
     for (const s of line.trim().split(',')) {
         if(s.trim() != '')
             HK.push(s.trim())
     }
-    // console.log(line.trim())
 }
-// console.log(JSON.stringify(HK, null, 2));
 for (const h of HK) {
     // run os command
     if(h.indexOf('"') > -1) {
-        console.log(`node download.js '${h}'`)    
+        console.log(`node download.js '${h}' &`)    
     } else {
-        console.log(`node download.js "${h}"`)
+        console.log(`node download.js "${h}" &`)
     }
-    // console.log(`node download.js '${h}'`)
-    // console.log(`node download.js "${h.replace('"', '\"')}"`)
 }
