ed1afb5b55a68cf043a58d85a9e988e567f3c1e3
Yotam Lichter <yotmail@gmail.com>
2022-07-11 21:28:56 +0000

    basic-site

---
+++ basic-stat/src/App.js
@@ -0,0 +1,161 @@
+import React from 'react';
+import { MDBDataTable } from 'mdbreact';
+import basicStatTableData from './basicStatTableData.json'
+
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const LABELS = ["HAPPY", "SAD", "ANGRY", "SURPRISED", "DISGUSTED", "FEAR", "CONFUSED", "CALM"]
+const LABEL_TO_HEBREW = {
+    "HAPPY": "שמחה", 
+    "SAD": "עצבות", 
+    "ANGRY": "כעס", 
+    "SURPRISED": "הפתעה",
+    "DISGUSTED": "גועל", 
+    "FEAR": "פחד", 
+    "CONFUSED": "בלבול", 
+    "CALM": "רוגע"
+}
+const LABEL_TO_COLOR = {
+    "HAPPY": "185,70,150", 
+    "SAD": "153,179,194", 
+    "ANGRY": "220,61,44", 
+    "SURPRISED": "179,205,70",
+    "DISGUSTED": "114,80,67", 
+    "FEAR": "243,180,70", 
+    "CONFUSED": "73,162,215", 
+    "CALM": "73,197,83"
+}
+
+const { rows, imageData, columns } = basicStatTableData
+rows.forEach(row => {
+    row.clickEvent = (row) => global.setData(row)
+})
+columns.forEach(column => {
+    column.label = LABEL_TO_HEBREW[column.label]
+})
+// basicStatTableData.rows = basicStatTableData.rows.reverse()
+
+
+
+const EmotionChart = ({data, showLegend, size}) => {
+    if(!data) return null
+    const pieData = {
+        labels: LABELS.map(label => LABEL_TO_HEBREW[label]),
+        datasets: [
+          {
+            data: LABELS.map(label => data[label]),
+            backgroundColor: LABELS.map(label => `rgba(${LABEL_TO_COLOR[label]}, 0.2)`),
+            borderColor: LABELS.map(label => `rgba(${LABEL_TO_COLOR[label]}, 1)`),
+            borderWidth: 1,
+          },
+        ],
+      };
+    const options = {
+        plugins: {
+          legend: {
+            display: showLegend,
+          },
+        },
+      };
+    return <div style={{width: size, height: size}}>
+        <Pie data={pieData} options={options} />
+    </div>
+}
+
+const ImagesViewer = ({ data}) => {
+    if(!data) return null
+    const {name} = data
+    const images = imageData[name]
+    return <div style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        padding: 64
+    }}>
+        {images.map(image => <div key={image.url} style={{
+            display: "flex",
+            marginTop: 32,
+            width: "100%",
+        }}>
+            <EmotionChart data={image} showLegend={false} size={200}/>
+            <div style={{
+                flex: 1
+            }}/>
+            <div>
+                <img src={image.url} height={200} />
+                <p style={{ 
+                    textAlign: "center",
+                }}>{image.url.split('_')[1].split('.')[0]}</p>
+            </div>
+            
+            <div style={{
+                flex: 1
+            }}/>
+        </div>)}
+    </div>
+}
+
+class DatatablePage extends React.Component {
+    constructor(props) {
+        super(props);
+        let curRow = rows[0]
+        console.log(window.location.hash)
+        if(window.location.hash) {
+            const hash = decodeURIComponent(window.location.hash).substring(1)
+            console.log(hash)
+            curRow = rows.find(row => row.name === hash)
+        }
+        this.state = {
+            data: curRow,
+            sortObject: { column: "HAPPY", direction: "asc" },
+        }
+        global.setData = data => {
+            window.location.hash = data.name
+            this.setState({ data })
+        }
+    }
+    render() {
+        return <div className="container-fluid">
+            <div className="row">
+                <div className="col-sm" style={{ 
+                    // backgroundColor: "rgba(255,0,0, 0.1)",
+                    marginTop: 32,
+                }}>
+                    <MDBDataTable
+                        striped
+                        bordered
+                        small
+                        data={basicStatTableData}
+                        entriesOptions={[15]}
+                        entries={15}
+                        noBottomColumns
+                        onSort={sortObject => this.setState({ sortObject })}
+                    />
+                </div>
+                <div className="col-sm" style={{ 
+                    // backgroundColor: "rgba(0,255,0, 0.1)",
+                    marginTop: 32,
+                }}>
+                    <h1 style={{ 
+                        textAlign: "center",
+                    }}>{this.state.data?.name}</h1>
+                    {/* <p style={{ 
+                        textAlign: "right",
+                    }}>ממוצע רגשות</p> */}
+                    <div style={{ 
+                        display: "flex",
+                        justifyContent: "center",
+                    }}>
+                    <EmotionChart data={this.state.data} showLegend={true} sortObject={this.state.sortObject} size={500}/>
+                    </div>
+                    <ImagesViewer data={this.state.data} />
+                </div>
+            </div>
+        </div>
+    }
+}
+
+export default DatatablePage;
---
+++ create_table_json.mjs
@@ -0,0 +1,71 @@
+// run faces analyter on AWS rekognition
+
+import fs from 'fs'
+
+
+const ROWS = []
+const folders = fs.readdirSync('images');
+const EMOTION = new Set()
+const imageData = {}
+
+for (const folder of folders) {
+    // check if it is a folder
+    if (!fs.lstatSync(`images/${folder}`).isDirectory()) continue
+    const files = fs.readdirSync(`images/${folder}`);
+    const data = {}
+    let numberOfSingleFaceImage = 0
+    const imgData = []
+    for (const file of files) {
+        if (!file.endsWith('.json')) continue
+        // console.log(`${folder}/${file}`);
+        const imageJsonData = JSON.parse(fs.readFileSync(`images/${folder}/${file}`, 'utf8'));
+        // detectFace(`${folder}/${file}`);
+        const numberOfFaces = imageJsonData.FaceDetails.length;
+        if(numberOfFaces !== 1) continue
+        const imageEmotion = {}
+        for (let i in imageJsonData.FaceDetails[0].Emotions) {
+            let { Type, Confidence } = imageJsonData.FaceDetails[0].Emotions[i];
+            EMOTION.add(Type)
+            if(!data[Type]) data[Type] = 0
+            Confidence = Math.round(Confidence * 100) / 100
+            data[Type] += Confidence
+            imageEmotion[Type] = Confidence
+        }
+        const url = `https://election-2022.s3.eu-west-1.amazonaws.com/${folder}/${file.replace('.json', '')}`
+        imgData.push({
+            url,
+            ...imageEmotion
+        })
+        numberOfSingleFaceImage++
+    }
+    imageData[folder] = imgData
+    // console.log(data)
+    for (let i in data) {
+        data[i] = data[i] / numberOfSingleFaceImage
+        // round number to 2 decimal places
+        data[i] = Math.round(data[i] * 100) / 100
+    }
+    data.name = folder
+    ROWS.push(data)
+
+}
+const EMOTION_LIST = [...EMOTION]
+const COLS = [{
+    label: 'name',
+    field: 'name',
+    sort: 'asc',
+}]
+
+for(const emotion of EMOTION_LIST) {
+    COLS.push({
+        label: emotion,
+        field: emotion,
+        sort: 'asc',
+    })
+}
+
+fs.writeFileSync('basic-stat/src/basicStatTableData.json', JSON.stringify({
+    rows: ROWS,
+    columns: COLS,
+    imageData
+}, null, 2));
